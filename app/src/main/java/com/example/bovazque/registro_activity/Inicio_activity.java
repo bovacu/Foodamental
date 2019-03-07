9738992b3d96b0d4d70fd01381a618a1fb81a8a3
bovacu <vazquezborja@hotmail.es>
2019-03-07 17:28:00 +0100

    -añadida la pantalla de inicio

--- app/src/main/java/com/example/bovazque/registro_activity/Inicio_activity.java
+++ app/src/main/java/com/example/bovazque/registro_activity/Inicio_activity.java
@@ -1,7 +1,9 @@
 package com.example.bovazque.registro_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Inicio_activity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class Inicio_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_activity);
+    }
+
+    public void goToInicioSesion(View view) {
+        Intent intent = new Intent(this, inicioSesion_activity.class);
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+
+    public void goToRegistro(View view) {
+        Intent intent = new Intent(this, Registro_activity.class);
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
