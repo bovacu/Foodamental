2f15bb6d7eb9c057247d8ddf6c80888483bde8c2
bovacu <vazquezborja@hotmail.es>
2019-03-07 17:43:04 +0100

    arregladas las pantallas + transicion del incio a inicio de sesion y registro

--- app/src/main/java/com/example/bovazque/registro_activity/inicioSesion_activity.java
+++ app/src/main/java/com/example/bovazque/registro_activity/inicioSesion_activity.java
@@ -2,12 +2,18 @@ package com.example.bovazque.registro_activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class inicioSesion_activity extends AppCompatActivity {
+
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio_sesion_activity);
+
+        this.email = findViewById(R.id.email_et);
     }
 }
