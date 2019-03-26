4e00c13dbcafc2571918837ddf5a91243f1ba599
bovacu <vazquezborja@hotmail.es>
2019-03-26 16:14:46 +0100

    -Persistencia

---
+++ app/src/main/java/com/example/bovazque/registro_activity/Persistencia.java
@@ -0,0 +1,26 @@
+package com.example.bovazque.registro_activity;
+
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Persistencia {
+
+    public static boolean nuevoUsuario(String nombre, String contrasena) {
+        return false;
+    }
+
+    public static boolean buscarUsuario(String nombre) {
+        return true;
+    }
+
+    public static boolean iniciarSesion(Usuario usuario) {
+        return true;
+    }
+
+    public static Usuario getUsuario(String nombre){
+        return null;
+    }
+}
