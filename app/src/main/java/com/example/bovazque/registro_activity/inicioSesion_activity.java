5c4511637c8804f8153a467177fc850bf30f10cc
caagudo <carlosap596@gmail.com>
2019-03-26 17:19:47 +0100

    new inicio_sesion()

--- app/src/main/java/com/example/bovazque/registro_activity/inicioSesion_activity.java
+++ app/src/main/java/com/example/bovazque/registro_activity/inicioSesion_activity.java
@@ -1,19 +1,99 @@
 package com.example.bovazque.registro_activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class inicioSesion_activity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
-    private EditText email;
+public class inicioSesion_activity extends AppCompatActivity  implements View.OnClickListener{
+
+
+    //defining view objects
+    private EditText TextEmail;
+    private EditText TextPassword;
+    private Button btnLogin;
+    private ProgressDialog progressDialog;
+
+
+    //Declaramos un objeto firebaseAuth
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inicio_sesion_activity);
 
-        this.email = findViewById(R.id.email_et);
+        //inicializamos el objeto firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //Referenciamos los views
+        TextEmail = (EditText) findViewById(R.id.email_et);
+        TextPassword = (EditText) findViewById(R.id.pasword_et);
+
+        btnLogin = (Button) findViewById(R.id.iniciobtn);
+
+        progressDialog = new ProgressDialog(this);
+
+        //attaching listener to button
+        btnLogin.setOnClickListener(this);
+    }
+    private void loguearUsuario() {
+        //Obtenemos el email y la contraseña desde las cajas de texto
+        final String email = TextEmail.getText().toString().trim();
+        String password = TextPassword.getText().toString().trim();
+
+        //Verificamos que las cajas de texto no esten vacías
+        if (TextUtils.isEmpty(email)) {//(precio.equals(""))
+            Toast.makeText(this, "Se debe ingresar un email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Falta ingresar la contraseña", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+        progressDialog.setMessage("Realizando consulta en linea...");
+        progressDialog.show();
+
+        //loguear usuario
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //checking if success
+                        if (task.isSuccessful()) {
+                            int pos = email.indexOf("@");
+                            String user = email.substring(0, pos);
+                            Toast.makeText(inicioSesion_activity.this, "Bienvenido: " + TextEmail.getText(), Toast.LENGTH_LONG).show();
+                            Intent intencion = new Intent(getApplication(), principal_activity.class);
+                            startActivity(intencion);
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {//si se presenta una colisión
+                                Toast.makeText(inicioSesion_activity.this, "Ese usuario ya existe ", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(inicioSesion_activity.this, "No se pudo registrar el usuario ", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        loguearUsuario();
     }
 }
